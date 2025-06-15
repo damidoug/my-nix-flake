f9c089bb8d7920114b27a8dc8b8b08d805aac05b
damidoug <contact@damidoug.dev>
2025-06-15 16:00:18 +0200

    add assertation for gui programs just work with host.isDesktop

--- modules/config/hosts.nix
+++ modules/config/hosts.nix
@@ -1,14 +1,16 @@
-{delib, ...}:
+{
+  delib,
+  lib,
+  ...
+}:
 delib.module {
   name = "hosts";
-
   options = with delib; let
     host = {config, ...}: {
       options =
         hostSubmoduleOptions
         // {
           type = noDefault (enumOption ["desktop" "server"] null);
-
           isDesktop = boolOption (config.type == "desktop");
           isServer = boolOption (config.type == "server");
         };
@@ -17,8 +19,16 @@ delib.module {
     host = hostOption host;
     hosts = hostsOption host;
   };
-
-  home.always = {myconfig, ...}: {
-    assertions = delib.hostNamesAssertions myconfig.hosts;
+  home.always = {myconfig, ...}: let
+    isAnyGuiAppEnabled = lib.any (app: app.enable or false) (lib.attrValues myconfig.gui);
+  in {
+    assertions =
+      (delib.hostNamesAssertions myconfig.hosts)
+      ++ [
+        {
+          assertion = !isAnyGuiAppEnabled || myconfig.host.isDesktop;
+          message = "GUI applications can only be enabled on a 'desktop' host.";
+        }
+      ];
   };
 }
